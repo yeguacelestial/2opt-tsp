fb33fd22dee95ded8e9562b46ebd9dbc963b66d7
yeguacelestial <yeguacelestial@gmail.com>
2020-04-13 16:01:25 -0500

    Changed obj_f to 'distance' and created main objects

--- two-op.py
+++ two-op.py
@@ -4,7 +4,9 @@
 #    2. Compute f(T) of current T
 #    3. 
 # b) First Found Strategy
+
 import sys
+import openpyxl
 
 def main():
     try:
@@ -39,9 +41,18 @@ def best_found(tour):
 def first_found(tour):
     return
 
-def obj_f(tour):
-    
-    return
+
+def distance(exl, i, j):
+    # Open Workbook object
+    wb_obj = openpyxl.load_workbook(exl)
+
+    # Get workbook active sheet object from active attribute
+    sheet_obj = wb_obj.active
+
+    # Cell object
+    cell_obj = sheet_obj.cell(row = i, column = j)
+
+    return print(cell_obj.value)
 
 if __name__ == '__main__':
     main()
